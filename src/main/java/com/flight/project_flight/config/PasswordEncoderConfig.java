4911c9987429fa80345bfd98fddf3d905e8bb266
essola <essolayk@yahoo.fr>
2024-12-31 21:48:55 +0100

    revision du passwordEncoder

--- src/main/java/com/flight/project_flight/config/PasswordEncoderConfig.java
+++ src/main/java/com/flight/project_flight/config/PasswordEncoderConfig.java
@@ -10,6 +10,6 @@ public class PasswordEncoderConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // Ou tout autre encodeur
+        return new BCryptPasswordEncoder();
     }
 }
